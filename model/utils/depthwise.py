4b1f60445cbec12823a3ef5ed7055567708a800c
vietnamican <vietnamican@gmail.com>
2020-07-17 23:12:58 +0700

    adding he initialize to depthwise

--- model/utils/depthwise.py
+++ model/utils/depthwise.py
@@ -14,9 +14,18 @@ class Depthwise(nn.Module):
                                 stride=stride, padding=padding, dilation=dilation, groups=in_channels, bias=True)
         self.pointwise = Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=1, padding=0,
                                 dilation=1)
+        self._init_weight()
 
     def forward(self, x):
         x = self.depthwise(x)
         x = self.pointwise(x)
 
         return x
+
+    def _init_weight(self):
+        for m in self.modules():
+            if isinstance(m, Conv2d):
+                nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
+            if isinstance(m, BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.fill_(0)
